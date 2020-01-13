bf7b25668c1b1317c5dd3a7dd5a345b887c89018
wangwj <34988153@qq.com>
2020-01-13 15:38:59 +0800

    生产、消费

--- src/com/athub/Main.java
+++ src/com/athub/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //生产、消费
+        //多线程 生产、消费
         String lock = new String("lock");
         P p = new P(lock);
         C c = new C(lock);
